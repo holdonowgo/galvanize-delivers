5b2431ee37de59da53c2d549ba4472ec2ece9b3e
Randall Spencer <randallspencer@gmail.com>
2017-01-30 19:51:44 -0800

    Do subtotal, tax and total calculations.

--- js/main.js
+++ js/main.js
@@ -40,9 +40,40 @@ window.onload = function() {
     let tdPrice = document.createElement('td');
     tdPrice.style.textAlign = "right";
     tdPrice.textContent = price;
+    tdPrice.setAttribute('class', 'itemPrice');
     tbody.appendChild(tr);
     tr.appendChild(tdItem);
     tr.appendChild(tdDummy);
     tr.appendChild(tdPrice);
+
+    updateTotals();
+  }
+
+  function updateTotals() {
+    let tax = 0;
+    let total = 0;
+    let subTotal = 0;
+    // let tbody = document.getElementsByTagName('tbody')[0];
+    let items = document.getElementsByClassName('itemPrice');
+    for(let item of items) {
+      let a = item.textContent;
+      a = a.split('');
+      a.shift();
+      a = a.join('');
+      a = parseFloat(a);
+      subTotal += a;
+      // console.log(subTotal);
+
+      let tdSubtotal = document.getElementById('subtotal');
+      let tdTax = document.getElementById('tax');
+      let tdTotal = document.getElementById('total');
+
+      tdSubtotal.textContent = '$' + subTotal;
+      tax = parseFloat((subTotal * salesTax).toFixed(2));
+      console.log(typeof tax);
+      tdTax.textContent = '$' + tax;
+      total = parseFloat(subTotal) + tax;
+      tdTotal.textContent = '$' + total;
+    }
   }
 }
